e04fc586b45009d26867cf50cf83c071e89ebac0
YairTawil <yairtawil@gmail.com>
2019-02-24 11:13:57 +0200

    revert config

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 const config = {
     port: process.env.PORT || 8080,
     backend: {
-        type: process.env.BACKEND_TYPE || 'mongodb',
+        type: process.env.BACKEND_TYPE || 'elasticsearch',
         hosts: process.env.BACKEND_HOSTS ? process.env.BACKEND_HOSTS.split(',') : ['ansyn.io'],
         credentials: process.env.BACKEND_CREDENTIALS || 'user:password',
-        schemaPrefix: process.env.BACKEND_SCHEMA_PREFIX || ''
+        schemaPrefix: process.env.BACKEND_SCHEMA_PREFIX || 'ansyn-dev-'
     }
 };
 
