b9531c01cf7b5831a4f82500f65312e468a1b0d6
asafMasa <asaf.masa@gmail.com>
2019-03-20 16:34:39 +0200

    fix default url

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const config = {
     port: process.env.PORT || 8080,
     backend: {
         type: process.env.BACKEND_TYPE || 'elasticsearch',
-        hosts: process.env.BACKEND_HOSTS ? process.env.BACKEND_HOSTS.split(',') : ['ansyn.io'],
+        hosts: process.env.BACKEND_HOSTS ? process.env.BACKEND_HOSTS.split(',') : ['platform.ansyn.io'],
         credentials: process.env.BACKEND_CREDENTIALS || 'user:password',
         schemaPrefix: process.env.BACKEND_SCHEMA_PREFIX || 'ansyn-dev-'
     }
