c5c6347e3cf434c0006d3f674d556c2edc62f79d
Tzahi Levi <thetzahilevi@gmail.com>
2019-12-29 11:43:51 +0200

    move elastic port to config (#14)

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 const config = {
     port: process.env.PORT || 8080,
     backend: {
+        elasticport: process.env.ELASTIC_PORT || 9200,
         type: process.env.BACKEND_TYPE || 'elasticsearch',
         hosts: process.env.BACKEND_HOSTS ? process.env.BACKEND_HOSTS.split(',') : ['platform.ansyn.io'],
         credentials: process.env.BACKEND_CREDENTIALS || 'user:password',
