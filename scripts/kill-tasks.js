409f40b8b28fb1b575e3a86bd00b4da3b0b63ce7
Tzahi Levi <thetzahilevi@gmail.com>
2020-08-31 15:40:27 +0300

    change cluster name

--- scripts/kill-tasks.js
+++ scripts/kill-tasks.js
@@ -5,12 +5,12 @@ if (process.argv.length <= 2) {
     process.exit(-1);
 }
 
-exec(`aws --region us-west-2 ecs list-tasks --cluster ansyn-app-cluster --service-name ${process.argv[2]}`, (error, stdout, stderr) => {
+exec(`aws --region us-west-2 ecs list-tasks --cluster mapathaon-ansyn --service-name ${process.argv[2]}`, (error, stdout, stderr) => {
 	const tasks = JSON.parse(stdout);
 	const arns = tasks.taskArns;
 	arns.forEach(arn => {
 		const task = arn.split('/')[1];
-		exec('aws --region us-west-2 ecs stop-task --cluster ansyn-app-cluster --task ' + task, (error, stdout, stderr) => {
+		exec('aws --region us-west-2 ecs stop-task --cluster mapathaon-ansyn --task ' + task, (error, stdout, stderr) => {
 			console.log(stdout);
 			console.log(stderr);
 			if (error !== null) {
